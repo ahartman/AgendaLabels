8a8034f585ac244aa6dbd87c967b161ccacba2fc
Andre Hartman <ahartman2@icloud.com>
2023-07-11 07:38:06 +0200

    fix

--- AgendaLabels/LabelsModel.swift
+++ AgendaLabels/LabelsModel.swift
@@ -10,22 +10,27 @@ import EventKit
 import UserNotifications
 
 class LabelsModel {
-    let labelNumbers = ["nrOfWeeks": 12, "nrPerWeek": 25, "nrPerDay": 6]
+    let labelNumbers = ["nrOfWeeks": 52, "nrPerWeek": 25, "nrPerDay": 6, "minimumPatients": 5]
 
-    var startCalendar: Date
+    let startCalendar: Date
     var endCalendar: Date
     var labelCalendars = [String: EKCalendar]()
     let eventStore = EKEventStore()
     let calendar = Calendar.current
     let dateFormatter = DateFormatter()
+    let defaults = UserDefaults.standard
 
     init() {
-        let calendar = Calendar.current
         let monday = DateComponents(hour: 0, minute: 0, second: 0, weekday: 2)
         startCalendar = calendar.nextDate(after: Date(), matching: monday, matchingPolicy: .nextTime, direction: .backward)!
         let dayComp = DateComponents(day: 7 * labelNumbers["nrOfWeeks"]!)
         endCalendar = calendar.date(byAdding: dayComp, to: startCalendar)!
+        let calendars = eventStore.calendars(for: .event).filter { $0.title.contains("Marieke") }
+        for calendar in calendars { labelCalendars[calendar.title] = calendar }
         dateFormatter.dateFormat = "d/M/yyyy"
+        if defaults.object(forKey: "today") == nil {
+            defaults.set(Date(), forKey: "today")
+        }
     }
 
     public func doLabels() async {
@@ -40,34 +45,63 @@ class LabelsModel {
             }
         }
 
-        let calendars = eventStore.calendars(for: .event).filter { $0.title == "Marieke" || $0.title == "Marieke blokkeren" || $0.title == "Marieke speciallekes" }
-        for calendar in calendars {
-            labelCalendars[calendar.title] = calendar
+        // handle expired first
+        if !calendar.isDateInToday(defaults.object(forKey: "today") as! Date) {
+            defaults.set(Date(), forKey: "today")
+            let expiredSessions = getProposedEvents()
+            let moveEvents = moveExpiredSessions(sessions: expiredSessions)
+            print("Verwijderd: \(moveEvents.count)")
+            if moveEvents.count > 0 {
+                showNotification(count: moveEvents.count)
+            }
+            for event in moveEvents {
+                try? eventStore.save(event, span: .thisEvent)
+            }
         }
 
-        let expiredSessions = getProposedEvents()
-        let moveLabels = moveExpiredSessions(sessions: expiredSessions)
-        for event in moveLabels {
-            try? eventStore.save(event, span: .thisEvent)
-        }
-
-        let (sessions, newSessions, oldLabels, oldEvents) = getLabelEvents()
-        let dayLabels = doDayCounts(sessions: sessions, newSessions: newSessions)
-        let weekLabels = doWeekCounts(sessions: sessions, newSessions: newSessions)
-
+        let (sessions, newSessions, proposedSessions, oldLabels, oldEvents) = getLabelEvents()
+        let weekLabels = doWeekLabels(sessions: sessions, newSessions: newSessions, proposedSessions: proposedSessions)
+        let newLabels = doWeekProposedLabels(newSessions: newSessions, proposedSessions: proposedSessions)
+        let dayLabels = doDayLabels(sessions: sessions, newSessions: newSessions, proposedSessions: proposedSessions)
         let oldStuff = oldLabels + oldEvents
+
         for event in oldStuff {
             try? eventStore.remove(event, span: .thisEvent)
-        }
-        for event in dayLabels! {
-            try? eventStore.save(event, span: .thisEvent)
         }
         for event in weekLabels! {
             try? eventStore.save(event, span: .thisEvent)
         }
-        try? eventStore.commit()
+        for event in dayLabels! {
+            try? eventStore.save(event, span: .thisEvent)
+        }
+        for event in newLabels {
+            try? eventStore.save(event!, span: .thisEvent)
+        }
 
-        showNotification(count: moveLabels.count)
+        try? eventStore.commit()
+    }
+
+    func getProposedEvents() -> [EKEvent] {
+        let proposedSessionsPredicate = eventStore.predicateForEvents(withStart: startCalendar, end: endCalendar, calendars: [labelCalendars["Marieke speciallekes"]!])
+        let proposedSessions = eventStore.events(matching: proposedSessionsPredicate).filter { $0.isAllDay == false && $0.title.contains("#") }
+        return (proposedSessions)
+    }
+
+    func moveExpiredSessions(sessions: [EKEvent]) -> [EKEvent] {
+        var localSessions = [EKEvent]()
+        for session in sessions {
+            if let location = session.location {
+                let year = calendar.dateComponents([.year], from: session.creationDate!).year!
+                if let date = dateFormatter.date(from: location + "/\(year)") {
+                    let numberOfDays = Calendar.current.dateComponents([.day], from: date, to: Date()).day!
+                    if numberOfDays > 14 {
+                        session.calendar = labelCalendars["Marieke blokkeren"]
+                        localSessions.append(session)
+                    }
+                }
+            }
+        }
+        return localSessions
     }
 
     func showNotification(count: Int) {
@@ -78,7 +112,7 @@ class LabelsModel {
         content.body = "\(count) \(tekst) verouderd."
         content.sound = UNNotificationSound.default
 
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1.0, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60.0, repeats: false)
         let request = UNNotificationRequest(identifier: "Identifier", content: content, trigger: trigger)
         center.add(request) { error in
             if let error = error {
@@ -87,55 +121,87 @@ class LabelsModel {
         }
     }
 
-    private func moveExpiredSessions(sessions: [EKEvent]) -> [EKEvent] {
-        var localSessions = [EKEvent]()
-
-        for session in sessions {
-            if let location = session.location {
-                let year = calendar.dateComponents([.year], from: session.creationDate!).year!
-                if let date = dateFormatter.date(from: location + "/\(year)") {
-                    let numberOfDays = Calendar.current.dateComponents([.day], from: date, to: Date()).day!
-                    if numberOfDays > 14 {
-                        // print("\(session.title),location: \(location), date: \(dateFormatter.string(from: date)),\(numberOfDays) \(session.calendar.title)")
-                        session.calendar = labelCalendars["Marieke blokkeren"]
-                        localSessions.append(session)
-                    }
-                }
-            }
-        }
-        return localSessions
-    }
-
-    private func getLabelEvents() -> ([EKEvent], [EKEvent], [EKEvent], [EKEvent]) {
+    func getLabelEvents() -> ([EKEvent], [EKEvent], [EKEvent], [EKEvent], [EKEvent]) {
         let oldLabelsStartDate = calendar.date(byAdding: DateComponents(day: -7), to: startCalendar)!
-        let labelsPredicate = eventStore.predicateForEvents(withStart: oldLabelsStartDate, end: endCalendar, calendars: [labelCalendars["Marieke"]!, labelCalendars["Marieke blokkeren"]!])
-        let labels = eventStore.events(matching: labelsPredicate).filter { $0.isAllDay == true }
+        let oldLabelsPredicate = eventStore.predicateForEvents(withStart: oldLabelsStartDate, end: endCalendar, calendars: [labelCalendars["Marieke"]!, labelCalendars["Marieke blokkeren"]!, labelCalendars["Marieke speciallekes"]!])
+        let oldLabels = eventStore.events(matching: oldLabelsPredicate).filter { $0.isAllDay == true }
 
         let sessionsPredicate = eventStore.predicateForEvents(withStart: startCalendar, end: endCalendar, calendars: [labelCalendars["Marieke"]!])
         let sessions = eventStore.events(matching: sessionsPredicate).filter { $0.isAllDay == false }
 
-        let newSessionsPredicate = eventStore.predicateForEvents(withStart: startCalendar, end: endCalendar, calendars: [labelCalendars["Marieke"]!, labelCalendars["Marieke speciallekes"]!])
+        let newSessionsPredicate = eventStore.predicateForEvents(withStart: startCalendar, end: endCalendar, calendars: [labelCalendars["Marieke"]!])
         let newSessions = eventStore.events(matching: newSessionsPredicate).filter { $0.isAllDay == false && $0.title.contains("#") }
+
+        let proposedSessionsPredicate = eventStore.predicateForEvents(withStart: startCalendar, end: endCalendar, calendars: [labelCalendars["Marieke speciallekes"]!])
+        let proposals = eventStore.events(matching: proposedSessionsPredicate).filter { $0.isAllDay == false && $0.title.contains("#") }
 
         let oldSessionsPredicate = eventStore.predicateForEvents(withStart: startCalendar, end: Date(), calendars: [labelCalendars["Marieke speciallekes"]!, labelCalendars["Marieke blokkeren"]!])
         let oldSessions = eventStore.events(matching: oldSessionsPredicate).filter { $0.isAllDay == false && $0.title.contains("#") }
 
-        return (sessions, newSessions, labels, oldSessions)
+        return (sessions, newSessions, proposals, oldLabels, oldSessions)
     }
 
-    private func getProposedEvents() -> [EKEvent] {
-        let proposedSessionsPredicate = eventStore.predicateForEvents(withStart: startCalendar, end: endCalendar, calendars: [labelCalendars["Marieke speciallekes"]!])
-        let proposedSessions = eventStore.events(matching: proposedSessionsPredicate).filter { $0.isAllDay == false && $0.title.contains("#") }
-        return (proposedSessions)
-    }
-
-    private func doDayCounts(sessions: [EKEvent], newSessions: [EKEvent]) -> [EKEvent]? {
+    func doWeekLabels(sessions: [EKEvent], newSessions: [EKEvent], proposedSessions: [EKEvent]) -> [EKEvent]? {
         var datum = startCalendar
         var tempLabels = [EKEvent]()
 
+        var weekCounts = [Int]()
+        var weekNewCounts = [Int]()
+        var weekProposalsCounts = [Int]()
+        var weekDates = [Date]()
+
+        while datum < endCalendar {
+            let datumWeek = calendar.component(.weekOfYear, from: datum)
+            weekCounts.append(sessions.filter {
+                let labelWeek = calendar.component(.weekOfYear, from: $0.startDate)
+                return datumWeek == labelWeek
+            }.count)
+
+            weekNewCounts.append(newSessions.filter {
+                let labelWeek = calendar.component(.weekOfYear, from: $0.startDate)
+                return datumWeek == labelWeek && $0.title.contains("#")
+            }.count)
+            weekProposalsCounts.append(proposedSessions.filter {
+                let labelWeek = calendar.component(.weekOfYear, from: $0.startDate)
+                return datumWeek == labelWeek && $0.title.contains("#")
+            }.count)
+            weekDates.append(datum)
+            datum = calendar.date(byAdding: DateComponents(day: 7), to: datum)!
+        }
+
+        let lastIndex = weekCounts.lastIndex(where: { $0 > labelNumbers["minimumPatients"]! })
+        weekCounts = Array(weekCounts[...lastIndex!])
+        endCalendar = calendar.date(byAdding: DateComponents(day: 7), to: weekDates[lastIndex!])!
+
+        var weekLabels = weekCounts.map { String($0) }
+        for (index, week) in weekCounts.enumerated() {
+            let event = EKEvent(eventStore: eventStore)
+            event.isAllDay = true
+            event.startDate = weekDates[index]
+            event.endDate = calendar.date(byAdding: DateComponents(day: 7), to: event.startDate)!
+            event.title = "Week \(weekLabels.joined(separator: ", "))"
+            if week > labelNumbers["nrPerWeek"]! {
+                event.calendar = labelCalendars["Marieke"]
+            } else {
+                event.calendar = labelCalendars["Marieke blokkeren"]
+            }
+            tempLabels.append(event)
+
+            weekLabels = Array(weekLabels.dropFirst())
+        }
+        return tempLabels
+    }
+
+    func doDayLabels(sessions: [EKEvent], newSessions: [EKEvent], proposedSessions: [EKEvent]) -> [EKEvent]? {
+        var datum = startCalendar
+        var localLabels = [EKEvent]()
+
         while datum <= endCalendar {
             let labelDayCount = sessions.filter { calendar.isDate(datum, inSameDayAs: $0.startDate) }.count
-            let labelDayNewPatient = newSessions.filter {
+            let labelDayNewCount = newSessions.filter {
+                calendar.isDate(datum, inSameDayAs: $0.startDate) && $0.title.contains("#")
+            }.count
+            let labelDayProposedcount = proposedSessions.filter {
                 calendar.isDate(datum, inSameDayAs: $0.startDate) && $0.title.contains("#")
             }.count
 
@@ -143,7 +209,6 @@ class LabelsModel {
             event.isAllDay = true
             event.startDate = datum
             event.endDate = datum
-
             if labelDayCount > labelNumbers["nrPerDay"]! {
                 event.title = "Vol \(labelDayCount)"
                 event.calendar = labelCalendars["Marieke"]
@@ -151,59 +216,64 @@ class LabelsModel {
                 event.title = "Dag \(labelDayCount)"
                 event.calendar = labelCalendars["Marieke blokkeren"]
             }
-            if labelDayNewPatient > 0 {
+            if labelDayNewCount > 0 {
                 event.title = "\(event.title!) #"
+                event.calendar = labelCalendars["Marieke speciallekes"]
             }
-
-            tempLabels.append(event)
+            if labelDayNewCount > 0 {
+                event.title = "Nieuwe #"
+                event.calendar = labelCalendars["Marieke speciallekes"]
+            } else if labelDayProposedcount > 0 {
+                event.title = "Voorstel #"
+                event.calendar = labelCalendars["Marieke speciallekes"]
+            }
+            localLabels.append(event)
             datum = calendar.date(byAdding: DateComponents(day: 1), to: datum)!
         }
-        return tempLabels
+        return localLabels
     }
 
-    private func doWeekCounts(sessions: [EKEvent], newSessions: [EKEvent]) -> [EKEvent]? {
+    func doWeekProposedLabels(newSessions: [EKEvent], proposedSessions: [EKEvent]) -> [EKEvent?] {
         var datum = startCalendar
-        var tempLabels = [EKEvent]()
-        var weekCounts = [Int]()
+        var localLabels = [EKEvent]()
         var weekNewCounts = [Int]()
+        var weekProposalsCounts = [Int]()
         var weekDates = [Date]()
+
+        endCalendar = max(
+            newSessions.count > 0 ? newSessions.last!.startDate : Date(),
+            proposedSessions.count > 0 ? proposedSessions.last!.startDate : Date()
+        )
         while datum < endCalendar {
             let datumWeek = calendar.component(.weekOfYear, from: datum)
-            let labelWeekCount = sessions.filter {
-                let labelWeek = calendar.component(.weekOfYear, from: $0.startDate)
-                return datumWeek == labelWeek
-            }.count
-            let labelWeekNewCount = newSessions.filter {
+            weekNewCounts.append(newSessions.filter {
                 let labelWeek = calendar.component(.weekOfYear, from: $0.startDate)
                 return datumWeek == labelWeek && $0.title.contains("#")
-            }.count
-            weekCounts.append(labelWeekCount)
-            weekNewCounts.append(labelWeekNewCount)
+            }.count)
+            weekProposalsCounts.append(proposedSessions.filter {
+                let labelWeek = calendar.component(.weekOfYear, from: $0.startDate)
+                return datumWeek == labelWeek && $0.title.contains("#")
+            }.count)
             weekDates.append(datum)
             datum = calendar.date(byAdding: DateComponents(day: 7), to: datum)!
         }
 
-        var weekLabels = weekCounts.map { String($0) }
         let weekNewLabels = weekNewCounts.map { String($0) }
+        let weekProposalsLabels = weekProposalsCounts.map { String($0) }
 
-        for (index, week) in weekCounts.enumerated() {
+        for (index, _) in weekNewLabels.enumerated() {
             let event = EKEvent(eventStore: eventStore)
             event.isAllDay = true
             event.startDate = weekDates[index]
             event.endDate = calendar.date(byAdding: DateComponents(day: 7), to: event.startDate)!
-            event.title = "Week \(weekLabels.joined(separator: ", "))" + " (\(weekNewLabels[index])#)"
-            if week > labelNumbers["nrPerWeek"]! {
-                event.calendar = labelCalendars["Marieke"]
-            } else {
-                event.calendar = labelCalendars["Marieke blokkeren"]
-            }
-            weekLabels = Array(weekLabels.dropFirst())
-            tempLabels.append(event)
+            event.title = "Nieuwe: \(weekNewLabels[index]), voorstellen: \(weekProposalsLabels[index])"
+            event.calendar = labelCalendars["Marieke speciallekes"]
+            localLabels.append(event)
         }
-        return tempLabels
+        return localLabels
     }
 
-    private func requestEventStoreAuthorization() async throws -> EKAuthorizationStatus {
+    func requestEventStoreAuthorization() async throws -> EKAuthorizationStatus {
         if try await eventStore.requestAccess(to: .event) {
             return EKEventStore.authorizationStatus(for: .event)
         } else {
@@ -211,7 +281,7 @@ class LabelsModel {
         }
     }
 
-    private enum EventError: Error, LocalizedError {
+    enum EventError: Error, LocalizedError {
         case unableToAccessCalendar
         case eventAuthorizationStatus(EKAuthorizationStatus? = nil)
 
